93defb0e35288731ead68c75c58f3e499b6061a9
Dakota Hawkins <dakotahawkins0110@gmail.com>
2018-08-02 17:09:14 -0400

    cleaned up the code

--- scripts/bash/undo_segregation.sh
+++ scripts/bash/undo_segregation.sh
@@ -8,24 +8,23 @@
 
 # example ./undo_segregation.sh <segegrated_qc> <qc>
 
+copy_fastq () {
+    local input_dir=$1
+    local output_dir=$2
+    local n_samples=`ls $input_dir | wc -l`
+    if [ $n_samples -gt 0 ];
+    then 
+        for sample in `find $input_dir/* -type d`
+        do
+            local id=`basename $sample`
+            new_loc=$output_dir"/$id/"
+            `cp -r $sample/*fastq.gz $new_loc`
+        done
+    fi
+}
 
-for sample in `find $1good/* -type d`
-do
-    id=`basename $sample`
-    new_loc=$2"/$id/"
-    `cp -r $sample/*.fastq.gz $new_loc`
-done
+copy_fastq "$1/good" $2
 
-for sample in `find $1bad/* -type d`
-do
-    id=`basename $sample`
-    new_loc=$2"/$id/"
-    `cp -r $sample/*.fastq.gz $new_loc`
-done
+copy_fastq "$1/ugly" $2
 
-for sample in `find $1ugly/* -type d`
-do
-    id=`basename $sample`
-    new_loc=$2"/$id/"
-    `cp -r $sample/*.fastq.gz $new_loc`
-done
+copy_fastq "$1/bad" $2
