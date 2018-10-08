2ab867ab3825728a5e1479f6b2ea2f42ee59a2e3
Dakota Hawkins <dakotahawkins0110@gmail.com>
2018-10-08 17:26:34 -0400

    Added pairwise Gtests with result plots.

--- scripts/python/cluster_cells.py
+++ scripts/python/cluster_cells.py
@@ -1,10 +1,11 @@
 import numpy as np 
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 import scanpy.api as sc
 import umap
 import louvain
-from scipy import spatial
+from scipy import spatial, stats
 
 sc.settings.verbosity = 1
 sc.settings.set_figure_params(dpi=80)
@@ -95,6 +96,7 @@ def filter_cells(anno_data, cell_col, values):
                   anno_data.obs.loc[x, cell_col] in values]
     return anno_data[keep_cells, ]
 
+
 def filter_genes(anno_data, gene_col, values):
     """
     Filter an annotated data frame to  cells only.
@@ -116,6 +118,156 @@ def filter_genes(anno_data, gene_col, values):
     keep_genes = [x for x in anno_data.var.index if\
                   anno_data.var.loc[x, gene_col] in values]
     return anno_data[:, keep_genes]
+
+
+def compare_clusters(anno_df, comparisons=None, cluster_col='louvain',
+                     compare_col='treatment'):
+    """
+    Compare cell make-up between identified clusters.
+    
+    Parameters
+    ----------
+    anno_df : sc.AnnData
+        Annotated dataframe containing cluster membership for each observation
+            and a second categorical variable to compare frequency against.
+    comparisons : list, optional
+        Post-hoc pairwise comparisons to perform. Elements of the list should be
+            length 2 containers containing categories to compare (e.g.
+            ('Control', 'Expermintanl')). Frequencies between conditions
+            will be compared in each cluster using a pairwise G-Test (the
+            default is None, which results in no post-hoc analysis). 
+    cluster_col : str, optional
+        Name of the column in `anno_df.obs` containing observation cluster/group
+            assignment (the default is 'louvain'.)
+    compare_col : str, optional
+        Name of the column in `anno_df.obs` partitioning observations into
+            groups of interest (the default is 'treatment', which compares
+            distribution of treatments within clusters.)
+    
+    Returns
+    -------
+    [type]
+        [description]
+    """
+    # check for cluster column
+    if cluster_col not in anno_df.obs.columns:
+        raise ValueError('No {} column in observation data.'.format(
+                         cluster_col))
+
+    # check for comparison column
+    if compare_col not in anno_df.obs.columns:
+        raise ValueError('No {} column in observations data.'.format(
+                         compare_col))
+    
+    #TODO check for comparisons in comparison column
+
+    count_table = pd.crosstab(anno_df.obs[cluster_col],
+                              anno_df.obs[compare_col])
+    
+    # calculate probabilities for comparison values
+    probabilites = np.sum(count_table, axis=0) / np.sum(count_table.values)
+
+    # calculate total number of cells per cluster in comparison
+    group_counts = np.sum(count_table, axis=1)
+    
+    # matrix multipy cluster counts and condition probabilities to get
+    # expected counts
+    expectation = group_counts.values.reshape((count_table.shape[0], 1))\
+                  @ probabilites.values.reshape((1, count_table.shape[1]))
+    expectation = pd.DataFrame(data=expectation, index=count_table.index,
+                               columns=count_table.columns)
+
+    # TODO do comparison off of original p-values
+    # TODO check pairwise implementation 
+    if comparisons is not None:
+        out = {}
+        for each in comparisons:
+            # perform g-test. TODO: look into degrees of freedom
+            results = stats.power_divergence(count_table[list(each)],
+                                             expectation[list(each)],
+                                             axis=1, lambda_='log-likelihood')
+
+            adj_pvals = results.pvalue * len(comparisons)\
+                                       * count_table.shape[0]
+
+            out['-'.join(list(each))] = {'observed': count_table[list(each)],
+                                         'expected': expectation[list(each)],
+                                         'pvals': results.pvalue,
+                                         'pvals.adj': adj_pvals}
+    else:
+        results = stats.power_divergence(count_table, expectation,
+                                            axis=1, lambda_='log-likelihood')
+        adj_pvals = results.pvalue * count_table.shape[0]
+
+        out = {'observed': count_table, 'expected': expectation,
+                'pvals': results.pvalue, 'pvals.adj': adj_pvals}
+
+    return out
+
+
+def plot_expectation_difference(observed, expected, normalize=False,
+                                compare_col='treatment', p_values=None):
+    """
+    Plot 
+    
+    Parameters
+    ----------
+    observed : [type]
+        [description]
+    expected : [type]
+        [description]
+    normalize : bool, optional
+        [description] (the default is False, which [default_description])
+    compare_col : str, optional
+        [description] (the default is 'treatment', which [default_description])
+    p_values : [type], optional
+        [description] (the default is None, which [default_description])
+    
+    Returns
+    -------
+    [type]
+        [description]
+    """
+
+    plt.style.use('fivethirtyeight')
+    if not normalize:
+        # raw difference
+        difference = observed - expected
+        ylab = 'Count Difference'
+    else:
+        # calculate percent error 
+        difference = (observed - expected) / (expected) * 100
+        ylab = 'Percent Error'
+        
+    difference['Cluster'] = difference.index.values
+    plot_data = pd.melt(difference, id_vars='Cluster')
+    figure = sns.barplot(x='Cluster', y='value', hue=compare_col,
+                         data=plot_data)
+
+    # fix axes
+    if normalize:
+        ylocs, ylabs = plt.yticks()
+        new_labs = ['{:.0f}%'.format(each) for each in ylocs]
+        plt.yticks(ylocs, new_labs)
+    plt.ylabel(ylab)
+
+    # fix legend
+    figure.legend_.set_title(compare_col[0].upper() + compare_col[1:])
+    figure.legend_.set_frame_on(False)
+
+    # add denetors for statistically significant deviations
+    if p_values is not None:
+        for i, p in enumerate(p_values):
+            if p < 0.5:
+                marker='*'
+                if p < 0.01:
+                    marker='**'
+                figure.annotate(marker,
+                                xy=(difference.index.values[i],
+                                    np.max(difference.iloc[i, :-1])),
+                                horizontalalignment='center')
+    
+    return figure
 
 def cluster_cells(anno_df, nn=15, metric='cosine'):
     sc.pp.neighbors(anno_df, n_neighbors=nn, metric=metric)
