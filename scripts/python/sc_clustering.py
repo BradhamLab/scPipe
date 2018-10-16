ad141d947a03487eeb6dcfe4dac5a8c797eb374b
Dakota Hawkins <dakotahawkins0110@gmail.com>
2018-10-16 15:03:36 -0400

    Documentation. Now cluster functions only.

--- scripts/python/sc_clustering.py
+++ scripts/python/sc_clustering.py
@@ -1,16 +1,13 @@
 import string
 
-import igraph
-import louvain
-import matplotlib.pyplot as plt
-from matplotlib.text import OffsetFrom
 import numpy as np
 import pandas as pd
 import scanpy.api as sc
-import seaborn as sns
 import sklearn
 import umap
 from scipy import spatial, stats
+
+import sc_utils
 
 sc.settings.verbosity = 1
 sc.settings.set_figure_params(dpi=80)
@@ -33,164 +30,6 @@ bad_cells = ["ASW_C07_2016-07-29", "ASW_C01_2016-07-29",
 
 subset_genes = ['SPU_022598', 'SPU_004767', 'SPU_000826', 'SPU_003102',
                 'SPU_028395', 'SPU_023386']
-
-def create_annotated_df(expr_file, gene_file, cell_file, filter_cells=None,
-                        transpose_expr=True):
-    """Create an AnnData object to hold expression, cell, and gene data.
-    
-    Parameters
-    ----------
-    expr_file : string
-        File path to csv file of expression data. Should be gene x cell.
-    gene_file : string
-        File path to gene annotation file. Should be gene x feature.
-    cell_file : string
-        File path to cell annotation file. Should be cell x feature. 
-    filter_cells : list, optional
-        Cells to remove from the final AnnData (the default is None, which
-        will not filter cells.)
-    tranpose_expr : boolean, optional
-        Whether to transpose the expression matrix. Set true if file is written
-        as (genes x sample) instead of (sample x gene). Default is True.
-    
-    Returns
-    -------
-    sc.AnnData
-        Annotated data frame containing expression data, cell data, and gene
-        data.
-    """
-
-    # read in data
-    expr_data = pd.read_csv(expr_file, index_col=0)
-    if transpose_expr:
-        expr_data = expr_data.T
-
-    gene_data = pd.read_csv(gene_file, index_col=0)
-    gene_data['scaffold'] = [x.replace('model', 'TU') for x in gene_data.index]
-    gene_data = gene_data.set_index('scaffold')  # this is filling with NaN
-
-    cell_data = pd.read_csv(cell_file, index_col=0)
-
-    # only keep cells found in both cell data and expr data
-    keep_cells = list(set(cell_data.index).intersection(expr_data.index))
-
-    # remove non-PMC cells
-    if filter_cells is not None:
-        keep_cells = [x for x in keep_cells if x not in filter_cells]
-        
-    cell_data = cell_data.filter(keep_cells, axis=0)
-    expr_data = expr_data.filter(keep_cells, axis=0)
-
-    # remove genes that are not in both annotation and and expression datasets 
-    keep_genes = list(set(expr_data.columns).intersection(gene_data.index))
-    gene_data = gene_data.filter(keep_genes, axis=0)
-    expr_data = expr_data.filter(keep_genes, axis=1)
-
-    # create anno df formatted cells x genes 
-    anno_df = sc.AnnData(expr_data.values, obs=cell_data, var=gene_data)
-    return anno_df
-
-def filter_cells(anno_data, cell_col, values):
-    """
-    Filter an annotated data frame to  cells only.
-    
-    Parameters
-    ----------
-    anno_data : sc.AnnData
-        Annotated data frame with cell annotations denoting treatment.
-    cell_col : string
-        Name of column in `sc.AnnData.obs` containing features to filter on.
-    values : list, string
-        list of strings denoting values of interest.
-    
-    Returns
-    -------
-    sc.AnnData
-        Annotated dataframe with samples filtered cells.
-    """
-    keep_cells = [x for x in anno_data.obs.index if\
-                  anno_data.obs.loc[x, cell_col] in values]
-    return anno_data[keep_cells, ]
-
-
-def filter_genes(anno_data, gene_col, values):
-    """
-    Filter an annotated data frame to  cells only.
-    
-    Parameters
-    ----------
-    anno_data : sc.AnnData
-        Annotated data frame with cell annotations denoting treatment.
-    gene_col : string
-        Name of column in `sc.AnnData.var` containing features to filter on.
-    values : list, string
-        list of strings denoting values of interest.
-    
-    Returns
-    -------
-    sc.AnnData
-        Annotated dataframe with samples filtered genes.
-    """
-    keep_genes = [x for x in anno_data.var.index if\
-                  anno_data.var.loc[x, gene_col] in values]
-    return anno_data[:, keep_genes]
-
-
-def dispersion(array):
-    """[summary]
-    
-    Parameters
-    ----------
-    array : [type]
-        [description]
-    
-    Returns
-    -------
-    [type]
-        [description]
-    """
-
-    quartiles = np.percentile(array, [25, 75])
-    return (quartiles[1] - quartiles[0]) / (quartiles[1] + quartiles[0])
-
-def variable_genes(anno_df, percentile=0.75, ignore_zeros=True):
-    """[summary]
-    
-    Parameters
-    ----------
-    anno_df : [type]
-        [description]
-    percentile : float, optional
-        [description] (the default is 0.75, which [default_description])
-    ignore_zeros : bool, optional
-        [description] (the default is True, which [default_description])
-    
-    Raises
-    ------
-    ValueError
-        [description]
-    
-    Returns
-    -------
-    [type]
-        [description]
-    """
-
-    gene_dispersion = np.zeros(anno_df.shape[1])
-    if not 0 < percentile < 1:
-        raise ValueError('`percentile` must be a value in between 0 and 1.')
-    if not ignore_zeros:
-        Warning("Keeping zeros when calculating dispersion often leads to NaNs.")
-    for i, gene in enumerate(anno_df.var.index):
-        expression = anno_df[:, gene].X
-        if ignore_zeros:
-            expression = expression[expression != 0]
-        if len(expression) > 0:
-            gene_dispersion[i] = dispersion(expression)
-    sorted_dispersion = np.argsort(gene_dispersion)
-    start_idx = int(len(sorted_dispersion)*percentile)
-
-    return anno_df.var.index.values[start_idx:]
 
 
 def compare_clusters(anno_df, comparisons=None, cluster_col='louvain',
@@ -225,8 +64,33 @@ def compare_clusters(anno_df, comparisons=None, cluster_col='louvain',
     
     Returns
     -------
-    [type]
-        [description]
+    dictionary
+        Dictionary containing comparison results between clusters including
+        G-tests of independence and, if a `comparisons` argument was provided,
+        pairwise Fisher exact test results.
+
+        key-value pairs:
+            'gtest': dictionary of Gtest results
+                key-value pairs:
+                    'observed': matrix of observed counts.
+                    'expected': matrix of expected counts.
+                    'pvals': pvalues for a GTest of independence following
+                        index order of 'observed' and 'expected' tables.
+                    'pvals.adj': adjusted p-values using the bonferonni
+                        correction.
+            'fisher': dictionary of pairwise comparison results. Results are
+                contained in a dictionary keyed by comparison groups separated
+                by a hyphen (e.g. if ['X', 'Y'] was provided as a comparison,
+                results of the comparison would be keyed 'X-Y').
+                
+                key-value pairs:
+                    'odds': numpy.array of odds ratios between comparisons
+                        ordered by cluster.
+                    'pvals': numpy.array of calculated p-values orderd by
+                        cluster.
+                    'pvals.adj': numpy.array of adjusted p-values by bonferonni
+                        ordered by cluster.
+                    'cluster': id denoting which cluster comparisons. 
     """
     # check for cluster column
     if cluster_col not in anno_df.obs.columns:
@@ -307,79 +171,27 @@ def compare_clusters(anno_df, comparisons=None, cluster_col='louvain',
             
     return out
 
-def plot_log_odds(fisher_out):
     
-    comparisons = list(fisher_out.keys())
-    sorted_comparisons = sorted(comparisons)
-    plot_data = pd.DataFrame(fisher_out[comparisons[0]])
-    plot_data['Comparison'] = comparisons[0]
-    for key in comparisons[1:]:
-        new_df = pd.DataFrame(fisher_out[key])
-        new_df['Comparison'] = key
-        plot_data = pd.concat((plot_data, new_df), axis=0)
-
-    cyndi = True
-    if cyndi:
-        cyndi_format(len(comparisons))
-        plt.grid(False)
-        plt.rc('axes.spines', top=False, bottom=False, right=False,
-            left=False)
-    figure = sns.barplot(data=plot_data, x='cluster', y='odds',
-                         hue='Comparison', hue_order=sorted_comparisons)
-    plt.axhline(y=1, xmax=plot_data.shape[0], linestyle='--')
-
-    xdiv = 1 / (2*(len(comparisons)) + 1)
-    midpoint = len(comparisons) / 2
-    for i in range(plot_data.shape[0]):
-        row = plot_data.iloc[i, :]
-        if row['pvals.adj'] < 0.05:
-            marker='*'
-            if row['pvals.adj'] < 0.01:
-                marker='**'
-            pt = np.where(row['Comparison']==np.array(sorted_comparisons))[0][0]
-            text_div = 0
-            if pt + 1 > midpoint:
-                text_div = (pt) * xdiv
-            elif pt + 1 < midpoint:
-                text_div = -1 * (pt + 1) * xdiv
-            elif pt + 1 == midpoint and len(comparisons) % 2 == 0:
-                text_div = -1 * (pt + 1) * xdiv
-            print(text_div)
-            figure.annotate(marker,
-                            xy=(row.name + text_div, row['odds']),
-                            horizontalalignment='center')
-
-    plt.xlabel('Odds Ratio')
-    plt.ylabel('Cluster')
-
-    
-
-
-
-
-
-
-
-
 def cluster_cells(anno_df, nn=15, metric='cosine'):
-    """[summary]
+    """
+    Cluster cells using Louvain modularity on UMAP projections of cells.
     
     Parameters
     ----------
-    anno_df : [type]
-        [description]
+    anno_df : sc.AnnData
+        Annotated dataframe containing expression data
     nn : int, optional
-        [description] (the default is 15, which [default_description])
+        Number of neighbors to use in UMAP projections. Default is 15.
     metric : str, optional
-        [description] (the default is 'cosine', which [default_description])
+        Distance metric to use. Default is 'cosine'.
     
     Returns
     -------
-    [type]
-        [description]
+    sc.AnnData
+        Annotated dataframe with clustering results.
     """
 
-    sc.pp.neighbors(anno_df, n_neighbors=nn, metric=metric, use_rep='X')
+    sc.pp.neighbors(anno_df, n_neighbors=nn, metric=metric)
     sc.tl.umap(anno_df, min_dist=0.0)
     sc.tl.louvain(anno_df)
     anno_df.obs['umap1'] = anno_df.obsm['X_umap'][:, 0]
@@ -390,19 +202,21 @@ def cluster_cells(anno_df, nn=15, metric='cosine'):
 
 
 def rename_clusters(anno_df, cluster_col='louvain'):
-    """[summary]
+    """
+    Rename cluster ids to single letter ids.
     
     Parameters
     ----------
-    anno_df : [type]
-        [description]
+    anno_df : sc.AnnData
+        Annotated data frame with clustered cells.
     cluster_col : str, optional
-        [description] (the default is 'louvain', which [default_description])
+        Column in observations data (`anno_df.obs`) containing cluster
+        assignments.
     
     Returns
     -------
-    [type]
-        [description]
+    sc.AnnData
+        Annotated dataframe with re-named cluster ids.
     """
 
     clusters = sorted(anno_df.obs[cluster_col].values.unique())
@@ -415,19 +229,27 @@ def rename_clusters(anno_df, cluster_col='louvain'):
 
 
 def project_onto_controls(anno_df, neighbors=15):
-    """[summary]
+    """
+    Cluster cells by first learning a projection using control cells. 
+
+    Cluster cells using Louvain modularity on UMAP projected cells. However, 
+    UMAP projection is first learned using only control cells. All cells
+    are then transformed into the learned space, and clustered using Louvain
+    modularity. 
     
     Parameters
     ----------
-    anno_df : [type]
-        [description]
+    anno_df : sc.AnnData
+        Annotated dataframe of expression data.
     neighbors : int, optional
-        [description] (the default is 15, which [default_description])
+        Number of neighbors to use when learning UMAP transformation. Default is
+        15.
     
     Returns
     -------
-    [type]
-        [description]
+    sc.AnnData
+        Annotated dataframe with clustered cells. Clusters found in 'louvain'
+        column in observation dataframe.
     """
 
     control_anno = filter_cells(anno_df, 'treatment', ['ASW', 'DMSO'])
@@ -446,15 +268,3 @@ def project_onto_controls(anno_df, neighbors=15):
     sc.tl.louvain(anno_df, adjacency=neighbor_graph)
     anno_df = rename_clusters(anno_df, 'louvain')
     return anno_df
-
-
-
-
-    # plt.scatter(cell_umap.embedding_[:, 0], cell_umap.embedding_[:,1], c='blue')
-    # plt.scatter(treatment_cords[:, 0], treatment_cords[:, 1], c='red')
-
-
-    # umap_df = pd.DataFrame(umap_cords, columns=['umap1', 'umap2'])
-    # color_dict = {'ASW': 'blue', 'DMSO': 'red'}
-    # umap_df.plot.scatter('umap1', 'umap2', c=[color_dict[x] for x in control_anno.obs['treatment']])
-    # plt.show()
